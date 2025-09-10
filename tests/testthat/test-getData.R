b5cd0570e90aa9d10d17876e4aca8ddbd68dcdd7
HWu <apoptosisneuro@gmail.com>
2025-09-10 22:50:52 +0200

    make sure R6class is not initialized/ empty

--- tests/testthat/test-getData.R
+++ tests/testthat/test-getData.R
@@ -13,13 +13,17 @@ for (i in seq_along(all_data$shorthand)) {
         skip_on_cran() # Skip this test on CRAN, because CRAN does not allow internet access needed for data
         d <- suppressMessages(getData(shorthand))
         expect_true( "R6" %in% class(d)) # Return value is R6class
+        expect_true(is.data.frame(d$data))     #R6class is not empty
+        expect_true(!is.null(d$title))
+        expect_true(!is.null(d$variable.description))
+        expect_true(length(d$variable.description) > 0)
     })
 }
 message("getData errors for nonexistent data")
 test_that("getData errors for invalid shorthand", {
     expect_error(getData("nonexistent-database"))
 })
-message("getData returns dataframe")
+message("getData returns dataframe if include.metadata = FALSE")
 for (i in seq_along(all_data$shorthand)) {
     shorthand <- all_data$shorthand[i]
     message(sprintf("[%d/%d] Testing getData for: %s", i, total, shorthand))
